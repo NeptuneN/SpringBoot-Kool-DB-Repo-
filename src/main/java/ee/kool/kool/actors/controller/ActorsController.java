2021dc5f074ffed76f3af51c18105ec0dafd450a
Kevin Klemm <Kevin.Klemm@tptlive.ee>
2021-10-19 21:18:39 +0300

    ActorService not finished

--- src/main/java/ee/kool/kool/actors/controller/ActorsController.java
+++ src/main/java/ee/kool/kool/actors/controller/ActorsController.java
@@ -1,4 +1,30 @@
 package ee.kool.kool.actors.controller;
 
+import ee.kool.kool.actors.dto.Actor;
+import ee.kool.kool.actors.service.ActorService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
 public class ActorsController {
+
+    @Autowired
+    ActorService actorService;
+
+    @GetMapping("/v1/actors")
+    List<Actor> all() {
+        log.info("elb");
+        return actorService.mockActors();
+    }
+
+    @GetMapping("/v1/actors/{id}")
+    Actor one(@PathVariable Integer id) {
+        return actorService.mockOneActor(id);
+    }
 }
---
+++ src/main/java/ee/kool/kool/actors/service/ActorService.java
@@ -0,0 +1,28 @@
+package ee.kool.kool.actors.service;
+
+
+import ee.kool.kool.actors.dto.Actor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ActorService {
+
+    public List<Actor> mockActors() {
+        List<Actor> actors = new ArrayList<>();
+        Actor actor1 = new Actor();
+        actor1.setId(1);
+        // not sure
+        return actors;
+    }
+
+    public Actor mockOneActor(Integer id) {
+        Actor actor2 = new Actor();
+        actor2.setId(id);
+        actor2.setForeName("Stacy");
+        actor2.setLastName("Jule");
+        return actor2;
+    }
+}
